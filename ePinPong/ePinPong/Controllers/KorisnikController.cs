b3ed13217dac220966242e73788948a1ea1a68e2
unknown <eadilovic1@etf.unsa.ba>
2026-08-09 11:39:24 +0200

    KorisnikController koristi ILeagueStandingsService

--- ePinPong/ePinPong/Controllers/KorisnikController.cs
+++ ePinPong/ePinPong/Controllers/KorisnikController.cs
@@ -17,12 +17,14 @@ namespace ePinPong.Controllers
         private readonly ApplicationDbContext _context;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IBracketService _bracketService;
+        private readonly ILeagueStandingsService _leagueStandingsService;
 
-        public KorisnikController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, IBracketService bracketService)
+        public KorisnikController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, IBracketService bracketService, ILeagueStandingsService leagueStandingsService)
         {
             _context = context;
             _userManager = userManager;
             _bracketService = bracketService;
+            _leagueStandingsService = leagueStandingsService;
         }
 
         // GET: /Korisnik/Index/some-guid-id
@@ -95,92 +97,21 @@ namespace ePinPong.Controllers
             }
 
             // Računanje plasmana u ligama koje je igrač igrao, igra ili će igrati
-            var ligeStandings = new List<KorisnikLigaStandingsViewModel>();
-            
             var sveLige = await _context.Lige
                 .Include(l => l.Turniri)
                     .ThenInclude(t => t.Registracije)
-                .Include(l => l.Turniri)
-                    .ThenInclude(t => t.Mecevi)
-                        .ThenInclude(m => m.Igrac1)
-                .Include(l => l.Turniri)
-                    .ThenInclude(t => t.Mecevi)
-                        .ThenInclude(m => m.Igrac2)
                 .ToListAsync();
 
             var relevantneLige = sveLige
                 .Where(l => l.Turniri.Any(t => t.Registracije.Any(r => r.KorisnikID == id)))
                 .ToList();
 
-            var sviKorisnici = await _context.Users.Where(u => u.Id != "SLOBODAN").ToListAsync();
-
+            var ligeStandings = new List<KorisnikLigaStandingsViewModel>();
+            
             foreach (var liga in relevantneLige)
             {
-                var tabeleMap = new Dictionary<string, LigaStandingsViewModel>();
-                foreach (var u in sviKorisnici)
-                {
-                    tabeleMap[u.Id] = new LigaStandingsViewModel
-                    {
-                        Korisnik = u,
-                        UkupnoBodova = 0,
-                        BrojOdigranihTurnira = 0,
-                        BodoviPoKolima = new List<int>()
-                    };
-                }
-
-                var zavrseniTurniri = liga.Turniri.Where(t => t.Status == StatusTurnira.Zavrsen).OrderBy(t => t.Kolo).ToList();
-                foreach (var turnir in zavrseniTurniri)
-                {
-                    var plasmani = _bracketService.IzracunajPlasman(turnir);
-                    foreach (var item in tabeleMap)
-                    {
-                        string uId = item.Key;
-                        var model = item.Value;
-                        var plasmanInfo = plasmani.FirstOrDefault(p => p.KorisnikId == uId);
-                        if (plasmanInfo != null)
-                        {
-                            model.UkupnoBodova += plasmanInfo.Bodovi;
-                            model.BrojOdigranihTurnira++;
-                            model.BodoviPoKolima.Add(plasmanInfo.Bodovi);
-                        }
-                        else
-                        {
-                            model.BodoviPoKolima.Add(0);
-                        }
-                    }
-                }
-
-                var sortiraniStandings = tabeleMap.Values
-                    .Where(v => v.BrojOdigranihTurnira > 0)
-                    .OrderByDescending(v => v.UkupnoBodova)
-                    .ToList();
-
-                var mojStandingIndex = sortiraniStandings.FindIndex(s => s.Korisnik.Id == id);
-                if (mojStandingIndex >= 0)
-                {
-                    var mojStanding = sortiraniStandings[mojStandingIndex];
-                    ligeStandings.Add(new KorisnikLigaStandingsViewModel
-                    {
-                        Liga = liga,
-                        Pozicija = mojStandingIndex + 1,
-                        UkupnoBodova = mojStanding.UkupnoBodova,
-                        BrojOdigranihTurnira = mojStanding.BrojOdigranihTurnira,
-                        UkupnoUcesnika = sortiraniStandings.Count,
-                        NijeZapoceo = false
-                    });
-                }
-                else
-                {
-                    ligeStandings.Add(new KorisnikLigaStandingsViewModel
-                    {
-                        Liga = liga,
-                        Pozicija = 0,
-                        UkupnoBodova = 0,
-                        BrojOdigranihTurnira = 0,
-                        UkupnoUcesnika = sortiraniStandings.Count,
-                        NijeZapoceo = true
-                    });
-                }
+                var standing = await _leagueStandingsService.GetPlayerStandingAsync(liga, id);
+                ligeStandings.Add(standing);
             }
 
             var viewModel = new KorisnikTurniriViewModel
